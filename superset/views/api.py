bc4d74620a3d10bf35cff0f4eb7b14767b01b1a1
Javy <xujavy@gmail.com>
2018-11-23 17:13:18 +0800

    update api

--- superset/views/api.py
+++ superset/views/api.py
@@ -23,6 +23,8 @@ import simplejson as json
 
 api = Api(app)
 
+parser = reqparse.RequestParser()
+
 def json_success(msg, status=200):
     # res = Response(json_msg, status=status, mimetype='application/json')
     return jsonify({"code":status, "result": msg})
@@ -46,9 +48,13 @@ class Databases(Resource):
 api.add_resource(Databases, '/api/v1/databases')
 
 # 获取Schemas
+
 class Schemas(Resource):
-    def get(self, database_name):
+    parser.add_argument('databasename', type=str)
+    def get(self):
         try:
+            args = parser.parse_args()
+            database_name = args['databasename']
             session = db.session
             curdatabase = session.query(models.Database).filter_by(database_name=database_name).first()
             schemas = curdatabase.all_schema_names()
@@ -57,12 +63,17 @@ class Schemas(Resource):
             return json_error_response(e)
         return json_success(msg={"schemas": json.dumps(schemas)})
 
-api.add_resource(Schemas, '/api/v1/databases/<database_name>')
+api.add_resource(Schemas, '/api/v1/getschemas')
 
 # 获取Tables
 class Tables(Resource):
-    def get(self, database_name, schema_name=None):
+    parser.add_argument('databasename', type=str)
+    parser.add_argument('schemaname', type=str)
+    def get(self):
         try:
+            args = parser.parse_args()
+            database_name = args['databasename']
+            schema_name = args['schemaname']
             session = db.session
             curdatabase = session.query(models.Database).filter_by(database_name=database_name).first()
             alltables = curdatabase.all_table_names(schema=schema_name)
@@ -71,12 +82,19 @@ class Tables(Resource):
             return json_error_response(e) 
         return json_success(msg={"tables": json.dumps(alltables)})
 
-api.add_resource(Tables, '/api/v1/databases/<database_name>/<schema_name>')
+api.add_resource(Tables, '/api/v1/gettables')
 
 # 获取ColNames
 class ColNames(Resource):
-    def get(self, database_name, table_name, schema_name=None):
+    parser.add_argument('databasename', type=str)
+    parser.add_argument('schemaname', type=str)
+    parser.add_argument('tablename', type=str)
+    def get(self):
         try:
+            args = parser.parse_args()
+            database_name = args['databasename']
+            schema_name = args['schemaname']
+            table_name = args['tablename']
             session = db.session
             curdatabase = session.query(models.Database).filter_by(database_name=database_name).first()
             colnames = curdatabase.get_columns(table_name, schema_name)
@@ -91,29 +109,41 @@ class ColNames(Resource):
             return json_error_response(e)
         return json_success(msg={"columns": json.dumps(tmp)})
 
-api.add_resource(ColNames, '/api/v1/databases/<database_name>/<schema_name>/<table_name>')
+api.add_resource(ColNames, '/api/v1/getcolnames')
 
 
 # 获取Datas
 class GetAllData(Resource):
-    def get(self, database_name, table_name, schema_name=None):
+    parser.add_argument('databasename', type=str)
+    parser.add_argument('schemaname', type=str)
+    parser.add_argument('tablename', type=str)
+    def get(self):
         try:
+            args = parser.parse_args()
+            database_name = args['databasename']
+            schema_name = args['schemaname']
+            table_name = args['tablename']
             session = db.session
             curdatabase = session.query(models.Database).filter_by(database_name=database_name).first()
-            # if schema_name is not None:
-            #     table_name = schema_name + "." + table_name
             datas = curdatabase.get_df('SELECT * FROM {0}'.format(table_name), schema_name)
         except Exception as e:
             logging.exception(e)
             return json_error_response(e)
         return json_success(msg={"datas": datas.to_json(orient='records')})
 
-api.add_resource(GetAllData, '/api/v1/sql/getalldata/<database_name>/<schema_name>/<table_name>')
+api.add_resource(GetAllData, '/api/v1/sql/getalldata')
 
 # 执行SQL
 class ExecuteSql(Resource):
-    def get(self, database_name, sql, schema_name=None):
+    parser.add_argument('databasename', type=str)
+    parser.add_argument('schemaname', type=str)
+    parser.add_argument('sql', type=str)
+    def get(self):
         try:
+            args = parser.parse_args()
+            database_name = args['databasename'] 
+            schema_name = args['schemaname']
+            sql = args['sql']
             session = db.session
             curdatabase = session.query(models.Database).filter_by(database_name=database_name).first()
             datas = curdatabase.get_df(sql, schema_name)
@@ -122,12 +152,12 @@ class ExecuteSql(Resource):
             json_error_response(e)
         return json_success(msg=datas.to_json(orient='records'))
 
-api.add_resource(ExecuteSql, '/api/v1/sql/executesql/<database_name>/<sql>')
+api.add_resource(ExecuteSql, '/api/v1/sql/executesql')
 
 
-parser = reqparse.RequestParser()
-parser.add_argument('data', type=str)
 class SavedSql(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('data', type=str)
     def get(self):
         try:
             args = parser.parse_args()
@@ -165,6 +195,7 @@ class GetDatamodel(Resource):
             return json_error_response(e)
         return json_success(msg={"datamodels": datas.to_json(orient='records')})
 api.add_resource(GetDatamodel, '/api/v1/datamodels')
+
 
 # test conn
 class TestConnection(Resource):
@@ -225,9 +256,9 @@ class TestConnection(Resource):
                 'The error message returned was:\n{}').format(e))
 api.add_resource(TestConnection, '/api/v1/testconn')
 
-parser = reqparse.RequestParser()
-parser.add_argument('data')
+
 class AddDatamodel(Resource):
+    parser.add_argument('data')
     def get(self):
         try:
             args = parser.parse_args()
@@ -265,10 +296,10 @@ class AddDatamodel(Resource):
 # http://172.16.151.188:8088/api/v1/datamodel/add?data={%22database_name%22:%22test6%22,%22sqlalchemy_uri%22:%22postgresql://rasdaman:XXXXXXXXXX@10.0.4.90:5432/RASBASE%22,%22created_by_fk%22:1,%22changed_by_fk%22:1,%22password%22:%2212345678%22,%22cache_timeout%22:40,%22extra%22:%22%22,%22select_as_create_table_as%22:true,%22allow_ctas%22:true,%22expose_in_sqllab%22:true,%22force_ctas_schema%22:%22%22,%22allow_run_async%22:true,%22allow_run_sync%22:true,%22allow_dml%22:true,%22perm%22:%22%22,%22verbose_name%22:%22%22,%22impersonate_user%22:false,%22allow_multi_schema_metadata_fetch%22:false}
 api.add_resource(AddDatamodel, '/api/v1/datamodels/add')
 
-parser = reqparse.RequestParser()
-parser.add_argument('id', type=int)
-parser.add_argument('data', type=str)
+
 class UpdateDatamodel(Resource):
+    parser.add_argument('id', type=int)
+    parser.add_argument('data', type=str)
     def get(self):
         try:
             args = parser.parse_args()
